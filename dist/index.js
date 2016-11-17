99429f4e9f8b1e081a03d0f87cdf3ece96f47fc0
sharkinsspatial <sharkinsgis@gmail.com>
2016-11-16 21:44:22 -0300

    First

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = getTilesSize;
+
+var _sphericalmercator = require('sphericalmercator');
+
+var _sphericalmercator2 = _interopRequireDefault(_sphericalmercator);
+
+var _lodash = require('lodash.range');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function getTilesSize(box, minzoom, maxzoom) {
+    var sphericalmercator = new _sphericalmercator2.default();
+    var levels = (0, _lodash2.default)(minzoom, maxzoom + 1);
+    var numTiles = levels.reduce(function (tiles, level) {
+        var bounds = sphericalmercator.xyz(box, level);
+        var boundTotal = (bounds.maxX - bounds.minX) * (bounds.maxY - bounds.minY);
+        var levelTotal = boundTotal === 0 ? 1 : boundTotal;
+        return tiles + levelTotal;
+    }, 0);
+    return numTiles * 60000;
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Sphericalmercator from 'sphericalmercator';
+import range from 'lodash.range';
+
+export default function getTilesSize(box, minzoom, maxzoom) {
+    const sphericalmercator = new Sphericalmercator();
+    const levels = range(minzoom, maxzoom + 1);
+    const numTiles = levels.reduce((tiles, level) => {
+        const bounds = sphericalmercator.xyz(box, level);
+        const boundTotal = (bounds.maxX - bounds.minX) *
+            (bounds.maxY - bounds.minY);
+        const levelTotal = boundTotal === 0 ? 1 : boundTotal;
+        return tiles + levelTotal;
+    }, 0);
+    return numTiles * 60000;
+}
+
